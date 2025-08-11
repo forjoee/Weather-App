16e39fbd6408d430c76aca2be34807db7c118391
forjoe <iosif.dahdal@gmail.com>
2025-08-11 22:27:54 +0300

    Small Changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '.env') });
 console.log('Loaded API_KEY:', process.env.API_KEY ? 'Yes' : 'No');
 
 const express = require('express');
